07bd117f2a6cc805babdc71f03c8ea67eecc504a
darren0061 <1148340992@qq.com>
2020-05-15 01:13:54 +0800

    gulimall-manager

---
+++ gulimall-manager/src/main/java/com/darren/gulimall/GulimallApplication.java
@@ -0,0 +1,13 @@
+package com.darren.gulimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallApplication.class, args);
+    }
+
+}
---
+++ gulimall-manager/src/test/java/com/darren/gulimall/GulimallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darren.gulimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
